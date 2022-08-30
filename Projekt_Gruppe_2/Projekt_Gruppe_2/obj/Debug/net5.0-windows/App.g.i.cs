ca75c9e2778bf33ae39c7a098755f2b514a9cf11
R3VI3V <mthofmann14@gmail.com>
2022-08-30 11:51:00 +0200

    Michael Test

--- Projekt_Gruppe_2/Projekt_Gruppe_2/obj/Debug/net5.0-windows/App.g.i.cs
+++ Projekt_Gruppe_2/Projekt_Gruppe_2/obj/Debug/net5.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace Projekt_Gruppe_2 {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace Projekt_Gruppe_2 {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "5.0.13.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
         public static void Main() {
             Projekt_Gruppe_2.App app = new Projekt_Gruppe_2.App();
             app.InitializeComponent();
