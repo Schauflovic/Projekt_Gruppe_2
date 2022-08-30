4a621ca502044ec86a96e354346482c626fabd1a
gina0123 <gina.schneider@students.hs-mainz.de>
2022-08-30 22:00:30 +0200

    Write/Read JSON

--- Projekt_Gruppe_2/Projekt_Gruppe_2/MainWindow.xaml.cs
+++ Projekt_Gruppe_2/Projekt_Gruppe_2/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,51 @@ namespace Projekt_Gruppe_2
     /// </summary>
     public partial class MainWindow : Window
     {
+        int fileCounter = 1;
+        
+        Message message = new Message()
+            {
+                TimestampUnix = ((DateTimeOffset)DateTime.Now).ToUnixTimeSeconds(),
+                Payload = "",
+                IPSender = "",
+                AliasSender = "",
+                DataFormat = "",
+                IPEmpfaenger = "",
+                Port = 900
+            };
+
+        
+
         public MainWindow()
+        {            
+            InitializeComponent();            
+        }
+
+        private void btnSenden_Click(object sender, RoutedEventArgs e)
         {
-            InitializeComponent();
+            message.Payload = textboxTest.Text;
+            string stringjson = JsonConvert.SerializeObject(message);
+            ausgabeTest.Text = stringjson;
+
+            using (StreamWriter file = File.CreateText(@"C:\Users\user\Documents\message" + fileCounter + ".txt"))
+            {
+                string _file = Convert.ToString(file);
+                JsonSerializer serializer = new JsonSerializer();
+                serializer.Serialize(file, message);
+            }
+        }
+
+        public static Message messageReader(string path)
+        {
+            string json = File.ReadAllText(path);
+            return JsonConvert.DeserializeObject<Message>(json);
+        }
+
+        private void btnLesen_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"C:\Users\user\Documents\message" + fileCounter + ".txt";
+            Message m = messageReader(path);
+            ausgabeTest.Text = m.Payload;
         }
     }
 }
